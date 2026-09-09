fb17d772b8b73616a28c510bd2cbf58fa0340ee4
Dhilfa <kmfathimadhilfa@gmail.com>
2026-09-10 04:10:31 +0530

    Update button_led.ino

--- 01-button-led/button_led.ino
+++ 01-button-led/button_led.ino
@@ -1,17 +1,17 @@
-const int ledpin = 18;
-const int buttonpin = 4; 
+const int ledpin = 18; //gave the variable ledpin an integer 18
+const int buttonpin = 4; //can change pin easily
 
-void setup() {
+void setup() { //run once when code starts
   Serial.begin(115200);
   pinMode(ledpin, OUTPUT);
   pinMode(buttonpin, INPUT_PULLUP);
 }
 
-void loop() {
-  int buttonmode = digitalRead(buttonpin);
+void loop() { //run in loop
+  int buttonmode = digitalRead(buttonpin); //put the state of button to buttonmode(new variable)
 
   if(buttonmode == LOW){
-    Serial.println("BUTTON SWITCHED");
+    Serial.println("BUTTON SWITCHED"); //checking if circuit closed-use serial monitor to view
     digitalWrite(ledpin, HIGH);
   }else{
     digitalWrite(ledpin,LOW);
